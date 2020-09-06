93fcc68afc3e546eb5daf9735fd2b11f49aedfe2
Abdullah <abdullah272056@gmail.com>
2020-09-06 19:20:55 +0600

    activity_main_design and recyclerview design

---
+++ app/src/main/java/com/example/reminderappdesign/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.reminderappdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    FloatingActionButton floatingActionButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
